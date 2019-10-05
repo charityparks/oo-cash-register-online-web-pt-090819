85f5fe82a7920ba16115e03f1c8c36a33f7b60d3
Charity Parks <tuppercharity@gmail.com>
2019-10-05 21:38:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,9 +14,7 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     self.total += (price * quantity)
     self.price << (price * quantity)
-    
-    
-  
-  # end
+    self.items.concat([title] * quantity)
+  end
 
 end
